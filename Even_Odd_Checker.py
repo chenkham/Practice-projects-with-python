dfce9119dd5328ff7435e47cec554225ef054fb1
chenkham <Chenkhamchowlu@gmail.com>
2025-05-26 12:35:25 +0530

    initial commit

---
+++ Even_Odd_Checker.py
@@ -0,0 +1,4 @@
+#simple even odd checker in python in 3line
+take_value=int(input("Enter a number to check even or odd: "))
+if take_value % 2 == 0:   print("Even number")
+else:  print("Odd number")
